fafa73ecd21ca364326f84e96397e63289cbbf90
Nicolas <nicolasm96@outlook.com>
2015-08-05 14:49:41 -0500

    Modificaciones digital ocean

--- conversor/header.php
+++ conversor/header.php
@@ -12,13 +12,13 @@
     <title>Conversor de bases</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="../assets/css/agency.css" rel="stylesheet">
+    <link href="assets/css/agency.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="../assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
