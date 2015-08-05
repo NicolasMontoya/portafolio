eb3d7becfaecc83db8a3cb78425cad69b2749817
Nicolas <nicolasm96@outlook.com>
2015-08-05 15:30:13 -0500

    Configuracion descripcion about

--- templates/about.php
+++ templates/about.php
@@ -9,7 +9,7 @@
                 </div>
             </div>
 				<br>
-				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pharetra mi vitae bibendum vehicula. Nunc nec ligula a nibh egestas luctus. Sed gravida tincidunt venenatis. Nulla vitae elit vel enim lacinia fermentum. Maecenas ac lobortis enim. Phasellus dictum congue eros at ultrices. Aliquam quis nunc eget leo condimentum consectetur vitae et felis. Pellentesque rhoncus neque mi, eget gravida sapien rutrum et. Donec ultrices felis finibus, lobortis justo euismod, interdum felis. Fusce ornare nec lacus quis consectetur. Nullam dignissim sapien a rhoncus interdum. Mauris hendrerit feugiat purus. Morbi sed vulputate mi. Vivamus dignissim, lorem quis convallis egestas, quam nisl vulputate sapien, et maximus tellus ligula non dui. </p>
+				<p> Mi nombre es nicolas, soy estudiante de ingeniería electrónica en la Universidad de Antioquia, este portafolio es una muestra del aprendizaje de algunas aplicaciones en php, mysql y html5. Espero que sea de su agrado. Si tienen alguna duda contactenme en mis redes sociales. Algunas paginas utiles que recomiendo para aprender son: <a href="http://www.platzi.com" target="_blank">Platzi</a>, <a href="http:// www.packagist.org" target="_blank">Packagist</a>, <a href="http:// www.php.net" target="_blank">PHP Documentación</a>. Buena suerte. </p>
 			</div>
 		</div>
 	</div>
