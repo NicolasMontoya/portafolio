447b17a1aa5d015cc13f0ab2630c2bc6201a1392
Nicolas <nicolasm96@outlook.com>
2015-08-05 18:52:38 -0500

    Decimales

--- models/ConversorBase.php
+++ models/ConversorBase.php
@@ -16,8 +16,21 @@ class ModelCB
 		if ($this->base == 2) {
 			$lenght = strlen($this->numero)-1;
 			for ($i=0; $i <= $lenght ; $i++) { 
+				if ($this->numero[$i]==',') {
+					$this->conversiones[0]=0;
+					$p=$i;
+					for ($j=0; $i <= $lenght ; $j++) { 
+						$i++;
+						$this->conversiones[0]=$this->conversiones[0]+(($this->numero[$i])*(pow(2,$j)));
+					}
+					for ($k=-1; $p >= 0 ; $k--) { 
+						$p--;
+						$this->conversiones[0]=$this->conversiones[0]+(($this->numero[$p])*(pow(2,$k)));
+					}
+				}
 				$this->conversiones[0]=$this->conversiones[0]+(($this->numero[$lenght-$i])*(pow(2,$i)));
 			}
+
 
 			return $this->conversiones;
 		}
