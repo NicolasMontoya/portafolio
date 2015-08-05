3470b867b0344e43cf844eb0d02fbaa0e2079549
Nicolas <nicolasm96@outlook.com>
2015-08-05 18:07:30 -0500

    Prueba2

--- formulario.php
+++ formulario.php
@@ -1,32 +1,7 @@
 <?php
 require 'conversor/header.php';
-require 'models/ConversorBase.php';
 ?>
 
-	<style type="text/css">
-	p{
-		font-size: 2em;
-		margin-left: 50px;
-	}
-	h1{
-		font-size: 2em; 
-	}
-	</style>
-	<section id="about">
-	<div class="container">
-			<div class="modal-body">
-				<div class="row">
-                <div class="col-sm-4">
-                    <span >Conversion a decimal: </span>
-                    <p><?php $modelo=new ModelCB($_POST['numero'],2);
-	$datos=$modelo->conversor();
-	echo $datos;?></p>
-                </div>
-            </div>
-			</div>
-	</div>
-
-</section>
 
 <?php
 require 'templates/footer.php';
